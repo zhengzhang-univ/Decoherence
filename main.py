67ecbb2c574b4b7d36699c22ee303459cdedfe7f
zhengzhang-univ <zheng_zhang1@alumni.brown.edu>
2022-05-06 16:47:57 +0200

    Friday

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
 from RadiationField import QuantumOscillators
 import h5py
-import matplotlib.pyplot as plt
-from RadiationField.visulization import plots_2d_in_3d, animation_imshow
+#import matplotlib.pyplot as plt
+#from RadiationField.visulization import plots_2d_in_3d, animation_imshow
 
 
 omega_list = list(np.array([2e6, 1e6],dtype=int))
@@ -10,7 +10,32 @@ omega_list = list(np.array([2e6, 1e6],dtype=int))
 def heavist_chi(c_list):
     return int(2*np.absolute(c_list[0])**2 + np.absolute(c_list[1])**2)
 
+c_list_1 = [1000,1000]
+two_ocsi_sys_1 = QuantumOscillators.Chi_analysis(omega_list, c_list_1, 20000, 1e-16)
+#N_and_Nchi_ts_1, C_chi_1, tlist = two_ocsi_sys_1.Average_N_decomposed_evolution(0,10000,1)
+dm_array, N_averg = two_ocsi_sys_1.density_matrix_evolution(0,10000,1, 'N')
+tlist = np.linspace(0,10000,1)
+with h5py.File("oscillators_1.hdf5", "w") as f:
+    f.create_dataset("t",data=tlist)
+    f.create_dataset("dm_array",data=dm_array)
+    f.create_dataset("N",data=N_averg)
+    f.create_dataset("c",data=c_list_1)
 
+"""
+n_2N_avg = 2*c_list_1[0]**2 + c_list_1[1]**2
+c_list_2 = [np.sqrt(n_2N_avg-100**2),100]
+c_list_3 = [100,np.sqrt(n_2N_avg-2*100**2)]
+
+
+two_ocsi_sys_2 = QuantumOscillators.Chi_analysis(omega_list, c_list_2, 20000, 1e-16)
+N_and_Nchi_ts_2, C_chi_2, tlist = two_ocsi_sys_2.Average_N_decomposed_evolution(0,10000,1)
+
+two_ocsi_sys_3 = QuantumOscillators.Chi_analysis(omega_list, c_list_3, 20000, 1e-16)
+N_and_Nchi_ts_3, C_chi_3, tlist = two_ocsi_sys_3.Average_N_decomposed_evolution(0,10000,1)
+
+"""
+
+"""
 #c_list = [5,4]
 #c_list = [3+4j,4]
 #c_list_1 = [0,10]
@@ -43,4 +68,4 @@ animation_imshow(np.absolute(dm_array))
 plt.plot(tlist,N_averg,label='from density matrix')
 plt.plot(tlist,N_and_Nchi_ts_3[:,0],label='from Chi synthesis')
 plt.legend()
-plt.show()
+"""
