a7424af06176b57e5a24eeaa98a4cebf4b5c8214
zhengzhang-univ <zheng_zhang1@alumni.brown.edu>
2022-05-10 18:41:20 +0200

    ...

--- RadiationField/QuantumOscillators.py
+++ RadiationField/QuantumOscillators.py
@@ -27,17 +27,17 @@ def solve_whole_system_and_save_3(chimax):
     rank=mpiutil.rank
     size=mpiutil.size
     nbatch = math.floor(chimax/size)
-    mpiutil.barrier()
     for i in range(nbatch+1):
+        mpiutil.barrier()
         chi: int = rank + size * i
         eigvals, eigvecs = solve_Chi_eigen_sys(chi)
         f1 = h5py.File('eigenvalues.hdf5', 'w', driver='mpio', comm=mpiutil._comm)
-        f1.create_dataset(str(chi), data=eigvals)
-        f1.close()
+        f1.create_dataset(str(chi), data=eigvals, chunks=True)
         f2 = h5py.File('eigenvectors.hdf5', 'w', driver='mpio', comm=mpiutil._comm)
-        f2.create_dataset(str(chi), data=eigvecs)
+        f2.create_dataset(str(chi), data=eigvecs, chunks=True)
+        mpiutil.barrier()
+        f1.close()
         f2.close()
-    mpiutil.barrier()
     return None
 
 def solve_whole_system_and_save_2(chimax):
