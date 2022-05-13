647e51f6c675b95b7692a6bcc15d6dd44935553a
zhengzhang-univ <zheng_zhang1@alumni.brown.edu>
2022-05-13 10:14:57 +0200

    ...

--- RadiationField/QuantumOscillators_parallel.py
+++ RadiationField/QuantumOscillators_parallel.py
@@ -40,6 +40,102 @@ class two_osci_solved():
             self.eig_vecs.append(eigen_vecs(chi))
         f1.close()
         f2.close()
+        mpiutil.barrier()
+        self.solve_initial_conditions()
+
+
+    def get_init_coeff(self, N, n):
+        return coherent_state(N,self.c_list[0])*coherent_state(n,self.c_list[1])
+
+    def Nmax(self,Chi):
+        return math.floor(Chi / 2)
+
+    def solve_initial_conditions(self):
+        def get_initial_condition(chi):
+            ind = math.floor(chi/mpiutil.size)
+            V = np.matrix(self.eig_vecs[ind])
+            g_i = np.einsum("ij,j -> i", np.array(V.H), self.init_coeff_lists[chi])
+            return g_i
+        Chi_array = list(np.arange(self.Chimax + 1))
+        self.init_cond_lists = mpiutil.parallel_map(get_initial_condition, Chi_array, method="alt")
+        print('The system has been initialized!')
+
+    def all_coeffs_t(self, t):
+        tt = self.factor * t
+        Chi_array = list(np.arange(self.Chimax + 1))
+        def linear_solver(Chi):
+            ind = math.floor(Chi / mpiutil.size)
+            Nmax = self.Nmax(Chi)
+            basis = np.exp(self.eig_vals[ind] * tt)
+            aux = np.einsum("ij, j, j -> i", self.eig_vecs[ind], self.init_cond_lists[Chi], basis)
+            N_avrg = sum(np.absolute(aux)**2 * np.arange(Nmax+1))
+            return aux, N_avrg
+        result = mpiutil.parallel_map(linear_solver, Chi_array, method="alt")
+        coeffs, N_avrg_decomp = list(zip(*result))
+        return coeffs, sum(N_avrg_decomp)
+
+    def turn_list_to_array(self, lists):
+        result = np.zeros((self.Nmax(self.Chimax) + 1, self.Chimax + 1), dtype=complex)
+        for i in range(self.Chimax + 1):
+            for j in range(math.floor(i / 2) + 1):
+                a, b = self.indices_lists[i][j]
+                result[a, b] = lists[i][j]
+        return result
+
+    def from_list_to_density_mat(self, lists, oscillator):
+        C_Nn = self.turn_list_to_array(lists)
+        #A,b=sympy.shape(C_Nn)
+        result = None
+        #photon_num_avrg = None
+        if oscillator == 'n':
+            result = np.array(np.matrix(C_Nn).H) @ C_Nn
+            #photon_num_avrg = float(sum([result[i,i]*i for i in range(b)]))
+        elif oscillator == 'N':
+            result = C_Nn @ np.array(np.matrix(C_Nn).H)
+            #photon_num_avrg = float(sum([result[i,i]*i for i in range(A)]))
+        return result #, photon_num_avrg
+
+    def density_matrix_t(self, t, oscillator):
+        clist, aux = self.all_coeffs_t(t)
+        return self.from_list_to_density_mat(clist,oscillator)
+
+    def density_matrix_evolution(self, st, et, dt, oscillator):
+        tlist = np.arange(st, et, dt)
+        dm_t=[]
+        N_t=[]
+        for t in tlist:
+            coeff_list, N_avrg = self.all_coeffs_t(t)
+            dm_t.append(self.from_list_to_density_mat(coeff_list,oscillator))
+            N_t.append(N_avrg)
+        return np.array(dm_t,dtype=complex), np.array(N_t, dtype=float)
+
+    def N_averg_evolution(self,st, et, dt):
+        tlist = np.arange(st, et, dt)
+        N_t=[]
+        for t in tlist:
+            coeff_list, N_avrg = self.all_coeffs_t(t)
+            N_t.append(N_avrg)
+        return  np.array(N_t)
+
+class two_osci_continue():
+    def __init__(self, omega_list, c_list, Chimax, Lambda, path, local_eig_vecs, local_eig_vals):
+        self.transfer_matrices = None
+        hb = sympy.N(hbar)
+        m_list = [hb * omega for omega in omega_list]
+        self.factor = float(
+            Lambda * math.sqrt(hb / (2 * m_list[0] * omega_list[0])) * (hb / (2 * m_list[1] * omega_list[1])) / hb) * (
+                          -1j)
+        self.Chimax = Chimax
+        self.c_list = np.array(c_list)
+        self.datapath = path
+        self.indices_lists = [[(N, Chi - 2 * N) for N in range(math.floor(Chi / 2) + 1)]
+                               for Chi in range(Chimax + 1)]
+        self.init_coeff_lists = [np.array([sympy.N(self.get_init_coeff(N, Chi - 2 * N))
+                                               for N in range(math.floor(Chi / 2) + 1)]).astype(complex)
+                                 for Chi in range(Chimax + 1)]
+        self.local_chis = mpiutil.partition_list_mpi(np.arange(Chimax+1), method="alt", comm=mpiutil._comm)
+        self.eig_vals = local_eig_vals
+        self.eig_vecs = local_eig_vecs
         self.solve_initial_conditions()
 
 
--- get_N_averg.py
+++ get_N_averg.py
@@ -5,7 +5,7 @@ import time
 st = time.time()
 
 omega_list = list(np.array([2e6, 1e6],dtype=int))
-c_list_1 = [10,10]
+c_list_1 = [35,35]
 c_list_2 = [15,np.sqrt(3675 - 2*15**2)]
 c_list_3 = [5,np.sqrt(3675 - 2*5**2)]
 c_list_4 = [0.5*np.sqrt(3675 - 5**2), 5]
@@ -17,13 +17,25 @@ enrg =  heavist_chi(c_list_1)
 path = "/data/zzhang/"
 #path = "/Users/zheng/Dropbox/project with Nick/"
 
-two_ocsi_sys = QuantumOscillators_parallel.two_osci_solved(omega_list, c_list_1, enrg*2, 1e-16, path)
-N_averg = two_ocsi_sys.N_averg_evolution(0,1000,1, 'N')
+sys1 = QuantumOscillators_parallel.two_osci_solved(omega_list, c_list_1, enrg*2, 1e-16, path)
+N_averg_1 = sys1.N_averg_evolution(0,1000,1)
+eig_vecs=sys1.eig_vecs
+eig_vals=sys1.eig_vals
+del sys1
+sys = QuantumOscillators_parallel.two_osci_continue(omega_list, c_list_2, enrg*2, 1e-16, path, eig_vecs, eig_vals)
+N_averg_2 = sys.N_averg_evolution(0,1000,1)
+sys = QuantumOscillators_parallel.two_osci_continue(omega_list, c_list_3, enrg*2, 1e-16, path, eig_vecs, eig_vals)
+N_averg_3 = sys.N_averg_evolution(0,1000,1)
+sys = QuantumOscillators_parallel.two_osci_continue(omega_list, c_list_4, enrg*2, 1e-16, path, eig_vecs, eig_vals)
+N_averg_4 = sys.N_averg_evolution(0,1000,1)
+
+
 
 et = time.time()
 if mpiutil.rank0:
     print("Elapsed time (s): {}".format(et - st))
-    tlist = np.linspace(0,800,1)
     with h5py.File(path+"N_averg.hdf5", "w") as f:
-        f.create_dataset("t",data=tlist)
-        f.create_dataset("N_sys1",data=N_averg)
+        f.create_dataset("N_sys1",data=N_averg_1)
+        f.create_dataset("N_sys2", data=N_averg_2)
+        f.create_dataset("N_sys3", data=N_averg_3)
+        f.create_dataset("N_sys4", data=N_averg_4)
