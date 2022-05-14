c194da472667ac83977e7c7a42561568982615ad
zhengzhang-univ <zheng_zhang1@alumni.brown.edu>
2022-05-15 00:07:45 +0200

    ...

--- get_N_averg.py
+++ get_N_averg.py
@@ -28,23 +28,22 @@ c_list_4 = get_c_list(chimax, 5)
 path = "/data/zzhang/"
 #path = "/Users/zheng/Dropbox/project with Nick/"
 t1 = time.time()
-sys1 = QuantumOscillators_parallel.two_osci_solved(omega_list, c_list_1, chimax, 1e-16, path)
+sys = QuantumOscillators_parallel.two_osci_solved(omega_list, c_list_1, chimax, 1e-16, path)
+N_averg_1 = sys.N_averg_evolution(0,1000,1)
 t2 = time.time()
-N_averg_1 = sys1.N_averg_evolution(0,1000,1)
+
+sys = QuantumOscillators_parallel.two_osci_solved(omega_list, c_list_2, chimax, 1e-16, path)
+N_averg_2 = sys.N_averg_evolution(0,1000,1)
+
+sys = QuantumOscillators_parallel.two_osci_solved(omega_list, c_list_3, chimax, 1e-16, path)
+N_averg_3 = sys.N_averg_evolution(0,1000,1)
+
+sys = QuantumOscillators_parallel.two_osci_solved(omega_list, c_list_4, chimax, 1e-16, path)
+N_averg_4 = sys.N_averg_evolution(0,1000,1)
 t3 = time.time()
 
-eig_vecs=sys1.eig_vecs
-eig_vals=sys1.eig_vals
-del sys1
-sys = QuantumOscillators_parallel.two_osci_continue(omega_list, c_list_2, chimax, 1e-16, path, eig_vecs, eig_vals)
-N_averg_2 = sys.N_averg_evolution(0,1000,1)
-sys = QuantumOscillators_parallel.two_osci_continue(omega_list, c_list_3, chimax, 1e-16, path, eig_vecs, eig_vals)
-N_averg_3 = sys.N_averg_evolution(0,1000,1)
-sys = QuantumOscillators_parallel.two_osci_continue(omega_list, c_list_4, chimax, 1e-16, path, eig_vecs, eig_vals)
-N_averg_4 = sys.N_averg_evolution(0,1000,1)
-
 if mpiutil.rank0:
-    print("time:{}".format([t2-t1,t3-t2]))
+    print("time:{}".format([t2-t1,t3-t1]))
     with h5py.File(path+"N_averg.hdf5", "w") as f:
         f.create_dataset("N_sys1",data=N_averg_1)
         f.create_dataset("N_sys2", data=N_averg_2)
