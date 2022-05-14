be0ae8d671a3f8858931b26459da214293b8ceb4
zhengzhang-univ <zheng_zhang1@alumni.brown.edu>
2022-05-14 13:32:50 +0200

    ...

--- EigenSolver.py
+++ EigenSolver.py
@@ -6,4 +6,4 @@ def get_total_nbytes(chimax):
     return np.sum(4*2*np.arange(np.floor(chimax/2)+1)**2)
 #parallel_solve_mixing_matrices_and_save(0,4000)
 path = "/data/zzhang/eigendata/"
-parallel_solve_mixing_matrices_and_save(0,10000,path)
+parallel_solve_mixing_matrices_and_save(0,4000,path)
