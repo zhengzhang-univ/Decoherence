ae2ddeda68b76559dfda11d6b1ea83f6f975f091
zhengzhang-univ <zheng_zhang1@alumni.brown.edu>
2022-05-15 00:10:51 +0200

    ...

--- get_N_averg.py
+++ get_N_averg.py
@@ -27,24 +27,28 @@ c_list_4 = get_c_list(chimax, 5)
 
 path = "/data/zzhang/"
 #path = "/Users/zheng/Dropbox/project with Nick/"
+st, et, dt = 0, 2000, 1
+
 t1 = time.time()
 sys = QuantumOscillators_parallel.two_osci_solved(omega_list, c_list_1, chimax, 1e-16, path)
-N_averg_1 = sys.N_averg_evolution(0,1000,1)
+N_averg_1 = sys.N_averg_evolution(st, et, dt)
 t2 = time.time()
 
 sys = QuantumOscillators_parallel.two_osci_solved(omega_list, c_list_2, chimax, 1e-16, path)
-N_averg_2 = sys.N_averg_evolution(0,1000,1)
+N_averg_2 = sys.N_averg_evolution(st, et, dt)
 
 sys = QuantumOscillators_parallel.two_osci_solved(omega_list, c_list_3, chimax, 1e-16, path)
-N_averg_3 = sys.N_averg_evolution(0,1000,1)
+N_averg_3 = sys.N_averg_evolution(st, et, dt)
 
 sys = QuantumOscillators_parallel.two_osci_solved(omega_list, c_list_4, chimax, 1e-16, path)
-N_averg_4 = sys.N_averg_evolution(0,1000,1)
+N_averg_4 = sys.N_averg_evolution(st, et, dt)
 t3 = time.time()
 
 if mpiutil.rank0:
     print("time:{}".format([t2-t1,t3-t1]))
+    ts = np.arange(st, et, dt)
     with h5py.File(path+"N_averg.hdf5", "w") as f:
+        f.create_dataset("time",data=ts)
         f.create_dataset("N_sys1",data=N_averg_1)
         f.create_dataset("N_sys2", data=N_averg_2)
         f.create_dataset("N_sys3", data=N_averg_3)
