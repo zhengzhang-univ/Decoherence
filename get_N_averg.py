b730379f3715a4a220c8e4821a23f6163762323c
zhengzhang-univ <zheng_zhang1@alumni.brown.edu>
2022-05-14 21:49:00 +0200

    ...

--- get_N_averg.py
+++ get_N_averg.py
@@ -20,10 +20,10 @@ def get_c_list(chimax, c):
     return [get_C(chimax,c),c]
 
 chimax=3999
-c_list_1 = [0,np.sqrt(3999/2.)]
-c_list_2 = get_c_list(chimax, 30)
+c_list_1 = get_c_list(chimax, 35)
+c_list_2 = get_c_list(chimax, 25)
 c_list_3 = get_c_list(chimax, 15)
-c_list_4 = get_c_list(chimax, 0)
+c_list_4 = get_c_list(chimax, 5)
 
 path = "/data/zzhang/"
 #path = "/Users/zheng/Dropbox/project with Nick/"
@@ -32,7 +32,7 @@ sys1 = QuantumOscillators_parallel.two_osci_solved(omega_list, c_list_1, chimax,
 t2 = time.time()
 N_averg_1 = sys1.N_averg_evolution(0,1000,1)
 t3 = time.time()
-"""
+
 eig_vecs=sys1.eig_vecs
 eig_vals=sys1.eig_vals
 del sys1
@@ -42,14 +42,11 @@ sys = QuantumOscillators_parallel.two_osci_continue(omega_list, c_list_3, chimax
 N_averg_3 = sys.N_averg_evolution(0,1000,1)
 sys = QuantumOscillators_parallel.two_osci_continue(omega_list, c_list_4, chimax, 1e-16, path, eig_vecs, eig_vals)
 N_averg_4 = sys.N_averg_evolution(0,1000,1)
-"""
 
 if mpiutil.rank0:
     print("time:{}".format([t2-t1,t3-t2]))
-    """
     with h5py.File(path+"N_averg.hdf5", "w") as f:
         f.create_dataset("N_sys1",data=N_averg_1)
         f.create_dataset("N_sys2", data=N_averg_2)
         f.create_dataset("N_sys3", data=N_averg_3)
         f.create_dataset("N_sys4", data=N_averg_4)
-    """
